1050f3d8648b0285b27e3535bd7137d8a774ec1f
JoyMbugua <mbuguajoy52@gmail.com>
2021-08-30 11:52:07 +0300

    added media queries

--- orders/views.py
+++ orders/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render
 
 from .models import Data, Order
 from .forms import DataModelForm
-import pandas as pd
-import time
 
-# Create your views here.
+
+
 def upload_file(request):
   form = DataModelForm(request.POST or None, request.FILES or None)
   if form.is_valid():
@@ -14,26 +13,11 @@ def upload_file(request):
     form = DataModelForm()
     # fetch unentered data
     unentered = Data.objects.get(entered=False) 
-    start = time.time()
+
     with open(unentered.file_name.path, 'r', encoding='latin-1') as file:
-      # df = pd.read_csv(file, sep=',')
-      # row_iterator = df.iterrows()
       
       Order.objects.from_csv(file, dict(invoice_no = 'InvoiceNo', stock_code='StockCode', description='Description', quantity='Quantity', customer_id='CustomerID', unit_price='UnitPrice', country='Country'))
       unentered.entered=True
       unentered.save()
-      end = time.time() - start
-      print("That took",end)
   return render(request, 'orders/upload_file.html', {'form': form})
 
-# objs = [
-
-#       Order(
-#           stock_code = row['StockCode'],
-#           description  = row['Description'],
-#           quantity  = row['Quantity'],
-#           unit_price  = row['UnitPrice'],
-#           country = row['Country']
-#       )
-#       for index, row in row_iterator
-#       ]
